e48b19e89d2bcc08df0dca4faeac29f45de08f08
Mauro Ribeiro <mauro0x52@gmail.com>
2013-03-12 15:09:51 -0300

    Ajustando o negocio de enviar email

--- application/controllers/helpers/EeMsg.php
+++ application/controllers/helpers/EeMsg.php
@@ -18,8 +18,8 @@ class Ee_Controller_Helper_EeMsg extends Zend_Controller_Action_Helper_Abstract
      * @author Mauro Ribeiro
      */
     private function sendEmail($mail, $category = false) {
+        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/ee.ini','production');
         if($config->login->sendgrid->user && $config->login->sendgrid->user != '' && $config->login->sendgrid->password && $config->login->sendgrid->password != '') {
-            $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/ee.ini','production');
             $login = array(
                 'auth' => 'login',
                 'username' => $config->login->sendgrid->user,
